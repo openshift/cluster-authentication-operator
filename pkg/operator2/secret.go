fba6289362adaba7d98454e3bf3836529e7d180a
Sally O'Malley <somalley@redhat.com>
2019-01-08 13:30:05 -0500

    validate session secrets

--- pkg/operator2/secret.go
+++ pkg/operator2/secret.go
@@ -3,6 +3,7 @@ package operator2
 import (
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 
 	"github.com/golang/glog"
@@ -13,33 +14,61 @@ import (
 	legacyconfigv1 "github.com/openshift/api/legacyconfig/v1"
 )
 
-func (c *osinOperator) expectedSessionSecret() *v1.Secret {
+func (c *osinOperator) expectedSessionSecret() (*v1.Secret, error) {
 	secret, err := c.secrets.Secrets(targetName).Get(targetName, metav1.GetOptions{})
-	if err != nil || isInvalidSessionSecret(secret) {
+	if err != nil || !isValidSessionSecret(secret) {
 		glog.V(4).Infof("failed to get secret %s: %v", targetName, err)
-		return randomSessionSecret()
+		generatedSessionSecret, err := randomSessionSecret()
+		if err != nil {
+			return nil, err
+		}
+		return generatedSessionSecret, nil
 	}
-	return secret
+	return secret, nil
 }
 
-func isInvalidSessionSecret(secret *v1.Secret) bool {
-	// TODO add validation
-	return false
+func isValidSessionSecret(secret *v1.Secret) bool {
+	// TODO add more validation?
+	var sessionSecretsBytes [][]byte
+	for _, v := range secret.Data {
+		sessionSecretsBytes = append(sessionSecretsBytes, v)
+	}
+	for _, ss := range sessionSecretsBytes {
+		var sessionSecrets *legacyconfigv1.SessionSecrets
+		err := json.Unmarshal(ss, &sessionSecrets)
+		if err != nil {
+			return false
+		}
+		for _, s := range sessionSecrets.Secrets {
+			if len(s.Authentication) != 64 {
+				return false
+			}
+
+			if len(s.Encryption) != 32 {
+				return false
+			}
+		}
+	}
+	return true
 }
 
-func randomSessionSecret() *v1.Secret {
+func randomSessionSecret() (*v1.Secret, error) {
+	skey, err := newSessionSecretsJSON()
+	if err != nil {
+		return nil, err
+	}
 	return &v1.Secret{
 		ObjectMeta: defaultMeta(),
 		Data: map[string][]byte{
-			sessionKey: newSessionSecretsJSON(),
+			sessionKey: skey,
 		},
-	}
+	}, nil
 }
 
-func newSessionSecretsJSON() []byte {
+func newSessionSecretsJSON() ([]byte, error) {
 	const (
-		sha256KeyLenBits = sha256.BlockSize * 8 // max key size with HMAC SHA256
-		aes256KeyLenBits = 256                  // max key size with AES (AES-256)
+		sha256KeyLenBytes = sha256.BlockSize // max key size with HMAC SHA256
+		aes256KeyLenBytes = 32               // max key size with AES (AES-256)
 	)
 
 	secrets := &legacyconfigv1.SessionSecrets{
@@ -49,28 +78,33 @@ func newSessionSecretsJSON() []byte {
 		},
 		Secrets: []legacyconfigv1.SessionSecret{
 			{
-				Authentication: string(randomBits(sha256KeyLenBits)), // TODO these lengths are probably wrong
-				Encryption:     string(randomBits(aes256KeyLenBits)),
+				Authentication: randomString(sha256KeyLenBytes), // 64 chars
+				Encryption:     randomString(aes256KeyLenBytes), // 32 chars
 			},
 		},
 	}
 	secretsBytes, err := json.Marshal(secrets)
 	if err != nil {
-		panic(err) // should never happen
+		return nil, err // should never happen
 	}
 
-	return secretsBytes
+	return secretsBytes, nil
 }
 
 // needs to be in lib-go
-func randomBits(bits int) []byte {
-	size := bits / 8
-	if bits%8 != 0 {
-		size++
-	}
+func randomBytes(size int) []byte {
 	b := make([]byte, size)
 	if _, err := rand.Read(b); err != nil {
 		panic(err) // rand should never fail
 	}
 	return b
+}
+
+// randomString uses RawURLEncoding to ensure we do not get / characters or trailing ='s
+func randomString(size int) string {
+	// each byte (8 bits) gives us 4/3 base64 (6 bits) characters
+	// we account for that conversion and add one to handle truncation
+	b64size := base64.RawURLEncoding.DecodedLen(size) + 1
+	// trim down to the original requested size since we added one above
+	return base64.RawURLEncoding.EncodeToString(randomBytes(b64size))[:size]
 }
