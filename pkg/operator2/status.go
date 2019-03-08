63b8b8502a1f0dda5a2cd1f60c08728e29363a8f
Stanislav Laznicka <slaznick@redhat.com>
2019-03-08 11:53:21 +0100

    Add version to clusteroperator
    Adds the version we get from the deployment to the clusteroperator
    and authetnication.operator statuses. Also delays the authentication.operator
    status updates to the end of Sync() so that we don't stomp on our own foot
    should we want to be calling either of the set*Status() methods.

--- pkg/operator2/status.go
+++ pkg/operator2/status.go
@@ -5,54 +5,40 @@ import (
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
 )
 
-func (c *authOperator) setFailingStatus(operatorConfig *operatorv1.Authentication, reason, message string) error {
-	failStatusFunc := func(status *operatorv1.OperatorStatus) error {
-		v1helpers.SetOperatorCondition(&status.Conditions,
-			operatorv1.OperatorCondition{
-				Type:    operatorv1.OperatorStatusTypeFailing,
-				Status:  operatorv1.ConditionTrue,
-				Reason:  reason,
-				Message: message,
-			})
-
-		v1helpers.SetOperatorCondition(&status.Conditions, operatorv1.OperatorCondition{
-			Type:   operatorv1.OperatorStatusTypeProgressing,
-			Status: operatorv1.ConditionFalse,
+func (c *authOperator) setFailingStatus(operatorConfig *operatorv1.Authentication, reason, message string) {
+	v1helpers.SetOperatorCondition(&operatorConfig.Status.Conditions,
+		operatorv1.OperatorCondition{
+			Type:    operatorv1.OperatorStatusTypeFailing,
+			Status:  operatorv1.ConditionTrue,
+			Reason:  reason,
+			Message: message,
 		})
 
-		v1helpers.SetOperatorCondition(&status.Conditions,
-			operatorv1.OperatorCondition{
-				Type:   operatorv1.OperatorStatusTypeAvailable,
-				Status: operatorv1.ConditionFalse,
-			})
+	v1helpers.SetOperatorCondition(&operatorConfig.Status.Conditions, operatorv1.OperatorCondition{
+		Type:   operatorv1.OperatorStatusTypeProgressing,
+		Status: operatorv1.ConditionFalse,
+	})
 
-		return nil
-	}
-
-	_, _, err := v1helpers.UpdateStatus(c.authOperatorConfigClient, failStatusFunc)
-	return err
+	v1helpers.SetOperatorCondition(&operatorConfig.Status.Conditions,
+		operatorv1.OperatorCondition{
+			Type:   operatorv1.OperatorStatusTypeAvailable,
+			Status: operatorv1.ConditionFalse,
+		})
 }
 
-func (c *authOperator) setAvailableStatus(operatorConfig *operatorv1.Authentication) error {
-	availStatusFunc := func(status *operatorv1.OperatorStatus) error {
-		v1helpers.SetOperatorCondition(&status.Conditions, operatorv1.OperatorCondition{
-			Type:   operatorv1.OperatorStatusTypeAvailable,
-			Status: operatorv1.ConditionTrue,
-		})
+func (c *authOperator) setAvailableStatus(operatorConfig *operatorv1.Authentication) {
+	v1helpers.SetOperatorCondition(&operatorConfig.Status.Conditions, operatorv1.OperatorCondition{
+		Type:   operatorv1.OperatorStatusTypeAvailable,
+		Status: operatorv1.ConditionTrue,
+	})
 
-		v1helpers.SetOperatorCondition(&status.Conditions, operatorv1.OperatorCondition{
-			Type:   operatorv1.OperatorStatusTypeProgressing,
-			Status: operatorv1.ConditionFalse,
-		})
+	v1helpers.SetOperatorCondition(&operatorConfig.Status.Conditions, operatorv1.OperatorCondition{
+		Type:   operatorv1.OperatorStatusTypeProgressing,
+		Status: operatorv1.ConditionFalse,
+	})
 
-		v1helpers.SetOperatorCondition(&status.Conditions, operatorv1.OperatorCondition{
-			Type:   operatorv1.OperatorStatusTypeFailing,
-			Status: operatorv1.ConditionFalse,
-		})
-
-		return nil
-	}
-
-	_, _, err := v1helpers.UpdateStatus(c.authOperatorConfigClient, availStatusFunc)
-	return err
+	v1helpers.SetOperatorCondition(&operatorConfig.Status.Conditions, operatorv1.OperatorCondition{
+		Type:   operatorv1.OperatorStatusTypeFailing,
+		Status: operatorv1.ConditionFalse,
+	})
 }
