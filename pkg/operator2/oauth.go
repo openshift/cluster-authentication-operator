d6ca980645944769619905bb9faebf425a361143
Monis Khan <mkhan@redhat.com>
2019-04-12 16:56:07 -0400

    Handle defaulting of top level config
    This allows us to treat the empty value as a valid state.
    
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/operator2/oauth.go
+++ pkg/operator2/oauth.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 
@@ -30,10 +31,16 @@ func (c *authOperator) handleOAuthConfig(
 	*configSyncData,
 	error,
 ) {
-	oauthConfig, err := c.oauth.Get(globalConfigName, metav1.GetOptions{})
+	oauthConfigNoDefaults, err := c.oauth.Get(globalConfigName, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		oauthConfigNoDefaults, err = c.oauth.Create(&configv1.OAuth{
+			ObjectMeta: defaultGlobalConfigMeta(),
+		})
+	}
 	if err != nil {
 		return nil, nil, nil, err
 	}
+	oauthConfig := defaultOAuthConfig(oauthConfigNoDefaults)
 
 	var accessTokenInactivityTimeoutSeconds *int32
 	timeout := oauthConfig.Spec.TokenConfig.AccessTokenInactivityTimeoutSeconds
@@ -162,4 +169,14 @@ func getCliConfigMap(completeConfigBytes []byte) *corev1.ConfigMap {
 func getMasterCA() *string {
 	ca := serviceCAPath // need local var to be able to take address of it
 	return &ca
+}
+
+func defaultOAuthConfig(oauthConfig *configv1.OAuth) *configv1.OAuth {
+	out := oauthConfig.DeepCopy() // do not mutate informer cache
+
+	if out.Spec.TokenConfig.AccessTokenMaxAgeSeconds == 0 {
+		out.Spec.TokenConfig.AccessTokenMaxAgeSeconds = 24 * 60 * 60 // 1 day
+	}
+
+	return out
 }
