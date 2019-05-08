d5f40d8ba753422cb584c26cac3eac31ede0d17f
Monis Khan <mkhan@redhat.com>
2019-05-08 15:34:59 -0400

    Move to monis.app/go
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/boilerplate/operator/filter.go
+++
@@ -1,7 +0,0 @@
-package operator
-
-import "github.com/openshift/cluster-authentication-operator/pkg/boilerplate/controller"
-
-func FilterByNames(names ...string) controller.Filter {
-	return controller.FilterByNames(nil, names...)
-}
