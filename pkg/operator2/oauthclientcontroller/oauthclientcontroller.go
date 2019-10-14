f48239383be5740d25b0a1b1856c9bee27a4104e
Stanislav Laznicka <slaznick@redhat.com>
2019-10-14 17:11:14 +0200

    random WIP stuff

--- pkg/operator2/oauthclientcontroller/oauthclientcontroller.go
+++ pkg/operator2/oauthclientcontroller/oauthclientcontroller.go
@@ -3,6 +3,7 @@ package oauthclientcontroller
 import (
 	"monis.app/go/openshift/controller"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	oauthv1 "github.com/openshift/api/oauth/v1"
@@ -13,10 +14,14 @@ import (
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
 )
 
+type EnsureOAuthClient func(oauthClient oauthclient.OAuthClientInterface, routeClient routeclient.RouteInterface, actualClient *oauthv1.OAuthClient) error
+
 type oauthClientsController struct {
 	operatorClient    v1helpers.OperatorClient
 	routeClient       routeclient.RouteV1Interface
 	oauthClientClient oauthclient.OAuthClientInterface
+
+	clientEnsureMap map[string]EnsureOAuthClient
 
 	eventRecorder events.Recorder
 }
@@ -26,22 +31,44 @@ func NewOAuthClientsController(
 	oauthClientClient oauthclient.OAuthClientInterface,
 	oauthClientInformer oauthinformer.OAuthClientInformer,
 	routeClient routeclient.RouteV1Interface,
+	clientEnsureMap map[string]EnsureOAuthClient,
+	eventRecorder events.Recorder,
 ) controller.Runner {
 	c := oauthClientsController{
 		operatorClient:    operatorClient,
 		oauthClientClient: oauthClientClient,
 		routeClient:       routeClient,
+
+		clientEnsureMap: clientEnsureMap,
+
+		eventRecorder: eventRecorder,
 	}
 
 	return controller.New("OAutClientsController", c)
 }
 
 func (c *oauthClientsController) Key(namespace, name string) (metav1.Object, error) {
-	return c.oauthClientClient.Get(name, metav1.GetOptions{})
+	obj, err := c.oauthClientClient.Get(name, metav1.GetOptions{})
+	if err != nil && errors.IsNotFound(err) {
+		return defaultOAuthClient(name), nil
+	}
+	return obj, err
 }
 
 func (c *oauthClientsController) Sync(obj metav1.Object) error {
-	_ = obj.(*oauthv1.OAuthClient)
+	oauthClient := obj.(*oauthv1.OAuthClient)
+	oauthClientCopy := oauthClient.DeepCopy()
 
-	return nil
+	return c.clientEnsureMap[oauthClientCopy.Name](c.oauthClientClient, c.routeClient, oauthClientCopy)
+}
+
+// defaultOAuthClient returns a stub *OAuthClient object which should always trigger
+// updates in the sync loop
+func defaultOAuthClient(name string) *oauthv1.OAuthClient {
+	// TODO: we may want a more general approach which would allow specifying a default func
+	return &oauthv1.OAuthClient{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
 }
