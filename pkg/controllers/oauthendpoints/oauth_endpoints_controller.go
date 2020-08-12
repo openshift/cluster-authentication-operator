66148e3e7a5369b53ffd8c1a70e34175ac0ab163
Siva <vareti@redhat.com>
2020-08-12 11:29:36 -0500

    add call back to get tls config for an endpoint

--- pkg/controllers/oauthendpoints/oauth_endpoints_controller.go
+++ pkg/controllers/oauthendpoints/oauth_endpoints_controller.go
@@ -1,15 +1,20 @@
 package oauthendpoints
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"net"
 	"strconv"
 
+	configv1informers "github.com/openshift/client-go/config/informers/externalversions/config/v1"
+	configv1lister "github.com/openshift/client-go/config/listers/config/v1"
 	routev1informers "github.com/openshift/client-go/route/informers/externalversions/route/v1"
 	routev1listers "github.com/openshift/client-go/route/listers/route/v1"
 	"github.com/openshift/library-go/pkg/controller/factory"
 	"github.com/openshift/library-go/pkg/operator/events"
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
+	"k8s.io/klog"
 
 	"github.com/openshift/cluster-authentication-operator/pkg/libs/endpointaccessible"
 
@@ -20,44 +25,66 @@ import (
 // NewOAuthRouteCheckController returns a controller that checks the health of authentication route.
 func NewOAuthRouteCheckController(
 	operatorClient v1helpers.OperatorClient,
+	secretInformerForNamespaces corev1informers.SecretInformer,
 	routeInformerNamespaces routev1informers.RouteInformer,
+	ingressInformerAllNamespaces configv1informers.IngressInformer,
 	recorder events.Recorder,
 ) factory.Controller {
+	secretLister := secretInformerForNamespaces.Lister()
+	secretInformer := secretInformerForNamespaces.Informer()
 	routeLister := routeInformerNamespaces.Lister()
 	routeInformer := routeInformerNamespaces.Informer()
+	ingressLister := ingressInformerAllNamespaces.Lister()
+	ingressInformer := ingressInformerAllNamespaces.Informer()
+
 	endpointListFunc := func() ([]string, error) {
 		return listOAuthRoutes(routeLister, recorder)
 	}
 
+	getTLSConfigFunc := func() (*tls.Config, error) {
+		return getOAuthRouteTLSConfig(secretLister, ingressLister, recorder)
+	}
+
 	return endpointaccessible.NewEndpointAccessibleController(
 		"OAuthRouteCheck",
-		operatorClient, endpointListFunc, []factory.Informer{routeInformer}, recorder)
+		operatorClient, endpointListFunc, getTLSConfigFunc, []factory.Informer{routeInformer, secretInformer, ingressInformer}, recorder)
 }
 
 // NewOAuthServiceCheckController returns a controller that checks the health of authentication service.
 func NewOAuthServiceCheckController(
 	operatorClient v1helpers.OperatorClient,
+	secretInformerForNamespaces corev1informers.SecretInformer,
 	corev1Informers corev1informers.Interface,
 	recorder events.Recorder,
 ) factory.Controller {
+	secretLister := secretInformerForNamespaces.Lister()
+	secretInformer := secretInformerForNamespaces.Informer()
 	serviceLister := corev1Informers.Services().Lister()
 	serviceInformer := corev1Informers.Services().Informer()
+
 	endpointsListFunc := func() ([]string, error) {
 		return listOAuthServices(serviceLister, recorder)
 	}
 
+	getTLSConfigFunc := func() (*tls.Config, error) {
+		return getOAuthEndpointTLSConfig(secretLister, recorder)
+	}
+
 	return endpointaccessible.NewEndpointAccessibleController(
 		"OAuthServiceCheck",
-		operatorClient, endpointsListFunc, []factory.Informer{serviceInformer}, recorder)
+		operatorClient, endpointsListFunc, getTLSConfigFunc, []factory.Informer{serviceInformer, secretInformer}, recorder)
 }
 
 // NewOAuthServiceEndpointsCheckController returns a controller that checks the health of authentication service
 // endpoints.
 func NewOAuthServiceEndpointsCheckController(
 	operatorClient v1helpers.OperatorClient,
+	secretInformerForNamespaces corev1informers.SecretInformer,
 	corev1Informers corev1informers.Interface,
 	recorder events.Recorder,
 ) factory.Controller {
+	secretLister := secretInformerForNamespaces.Lister()
+	secretInformer := secretInformerForNamespaces.Informer()
 	endpointsLister := corev1Informers.Endpoints().Lister()
 	endpointsInformer := corev1Informers.Endpoints().Informer()
 
@@ -65,9 +92,13 @@ func NewOAuthServiceEndpointsCheckController(
 		return listOAuthServiceEndpoints(endpointsLister, recorder)
 	}
 
+	getTLSConfigFunc := func() (*tls.Config, error) {
+		return getOAuthEndpointTLSConfig(secretLister, recorder)
+	}
+
 	return endpointaccessible.NewEndpointAccessibleController(
 		"OAuthServiceEndpointsCheck",
-		operatorClient, endpointsListFn, []factory.Informer{endpointsInformer}, recorder)
+		operatorClient, endpointsListFn, getTLSConfigFunc, []factory.Informer{endpointsInformer, secretInformer}, recorder)
 }
 
 func listOAuthServiceEndpoints(endpointsLister corev1listers.EndpointsLister, recorder events.Recorder) ([]string, error) {
@@ -123,6 +154,66 @@ func listOAuthRoutes(routeLister routev1listers.RouteLister, recorder events.Rec
 		return nil, fmt.Errorf("route status does not have host address")
 	}
 	return toHealthzURL(results), nil
+}
+
+func getOAuthRouteTLSConfig(secretLister corev1listers.SecretLister, ingressLister configv1lister.IngressLister, recorder events.Recorder) (*tls.Config, error) {
+	ingress, err := ingressLister.Get("cluster")
+	if err != nil {
+		recorder.Warningf("OAuthRouteSecret", "failed to get ingress config: %v", err)
+		return nil, err
+	}
+	if len(ingress.Spec.Domain) == 0 {
+		return nil, fmt.Errorf("ingress config domain cannot be empty")
+	}
+
+	routerSecret, err := secretLister.Secrets("openshift-authentication").Get("v4-0-config-system-router-certs")
+	if err != nil {
+		recorder.Warningf("OAuthRouteSecret", "failed to get oauth route ca cert: %v", err)
+		return nil, err
+	}
+
+	// find the domain that matches our route
+	if _, ok := routerSecret.Data[ingress.Spec.Domain]; !ok {
+		klog.Infof("unable to find router certs for domain %s", ingress.Spec.Domain)
+		return nil, nil
+	}
+
+	rootCAs := x509.NewCertPool()
+	if ok := rootCAs.AppendCertsFromPEM(routerSecret.Data[ingress.Spec.Domain]); !ok {
+		klog.Infof("failed to parse router certs for domain %s", ingress.Spec.Domain)
+		return nil, nil
+	}
+
+	return &tls.Config{
+		RootCAs: rootCAs,
+	}, nil
+}
+
+func getOAuthEndpointTLSConfig(secretLister corev1listers.SecretLister, recorder events.Recorder) (*tls.Config, error) {
+	serviceSecret, err := secretLister.Secrets("openshift-authentication").Get("v4-0-config-system-serving-cert")
+	if err != nil {
+		recorder.Warningf("OAuthEndpointSecret", "failed to get oauth endpoint ca cert: %v", err)
+		return nil, err
+	}
+
+	// find the domain that matches our route
+	if _, ok := serviceSecret.Data["tls.crt"]; !ok {
+		return nil, fmt.Errorf("unable to find service ca bundle")
+	}
+
+	rootCAs := x509.NewCertPool()
+	if ok := rootCAs.AppendCertsFromPEM(serviceSecret.Data["tls.crt"]); !ok {
+		return nil, fmt.Errorf("no certificates could be parsed from the service ca bundle")
+	}
+	return &tls.Config{
+		RootCAs: rootCAs,
+		// Specify a host name allowed by the serving cert of the
+		// endpoints to ensure that TLS validates successfully. The
+		// serving cert the endpoint uses does not include IP SANs
+		// so accessing the endpoint via IP would otherwise result
+		// in validation failure.
+		ServerName: "oauth-openshift.openshift-authentication.svc",
+	}, nil
 }
 
 func toHealthzURL(urls []string) []string {
--- pkg/libs/endpointaccessible/endpoint_accessible_controller.go
+++ pkg/libs/endpointaccessible/endpoint_accessible_controller.go
@@ -16,11 +16,14 @@ import (
 )
 
 type endpointAccessibleController struct {
+	name           string
 	operatorClient v1helpers.OperatorClient
 	endpointListFn EndpointListFunc
+	getTLSConfigFn EndpointTLSConfigFunc
 }
 
 type EndpointListFunc func() ([]string, error)
+type EndpointTLSConfigFunc func() (*tls.Config, error)
 
 // NewEndpointAccessibleController returns a controller that checks if the endpoints
 // listed by endpointListFn are reachable
@@ -28,12 +31,15 @@ func NewEndpointAccessibleController(
 	name string,
 	operatorClient v1helpers.OperatorClient,
 	endpointListFn EndpointListFunc,
+	getTLSConfigFn EndpointTLSConfigFunc,
 	triggers []factory.Informer,
 	recorder events.Recorder,
 ) factory.Controller {
 	c := &endpointAccessibleController{
+		name:           name,
 		operatorClient: operatorClient,
 		endpointListFn: endpointListFn,
+		getTLSConfigFn: getTLSConfigFn,
 	}
 
 	return factory.New().
@@ -51,6 +57,10 @@ func (c *endpointAccessibleController) sync(ctx context.Context, syncCtx factory
 		return err
 	}
 
+	client, err := c.buildTLSClient()
+	if err != nil {
+		return err
+	}
 	// check all the endpoints in parallel.  This matters for pods.
 	errCh := make(chan error, len(endpoints))
 	wg := sync.WaitGroup{}
@@ -67,17 +77,6 @@ func (c *endpointAccessibleController) sync(ctx context.Context, syncCtx factory
 			reqCtx, cancel := context.WithTimeout(ctx, 10*time.Second) // avoid waiting forever
 			defer cancel()
 			req.WithContext(reqCtx)
-
-			// we don't really care  if anyone lies to us. We aren't sending important data.
-			client := &http.Client{
-				Timeout: 5 * time.Second,
-				Transport: &http.Transport{
-					Proxy: http.ProxyFromEnvironment,
-					TLSClientConfig: &tls.Config{
-						InsecureSkipVerify: true,
-					},
-				},
-			}
 
 			resp, err := client.Do(req)
 			if err != nil {
@@ -99,4 +98,20 @@ func (c *endpointAccessibleController) sync(ctx context.Context, syncCtx factory
 	}
 
 	return utilerrors.NewAggregate(errors)
+}
+
+func (c *endpointAccessibleController) buildTLSClient() (*http.Client, error) {
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	if c.getTLSConfigFn != nil {
+		tlsConfig, err := c.getTLSConfigFn()
+		if err != nil {
+			return nil, err
+		}
+		transport.TLSClientConfig = tlsConfig
+	}
+	return &http.Client{Transport: transport}, nil
 }
