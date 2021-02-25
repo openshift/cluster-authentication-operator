1b5711aa555d6a2f9b29bada1b45e650d218c97e
Stanislav Laznicka <slaznick@redhat.com>
2021-02-25 10:15:48 +0100

    GetOAuthServerRoute: check err for api.notfound instead of os.notfound

--- pkg/controllers/common/route.go
+++ pkg/controllers/common/route.go
@@ -2,32 +2,33 @@ package common
 
 import (
 	"fmt"
-	"os"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	operatorv1 "github.com/openshift/api/operator/v1"
 	routev1 "github.com/openshift/api/route/v1"
 	routev1lister "github.com/openshift/client-go/route/listers/route/v1"
-
-	corev1 "k8s.io/api/core/v1"
 )
 
 func GetOAuthServerRoute(routeLister routev1lister.RouteLister, conditionPrefix string) (*routev1.Route, []operatorv1.OperatorCondition) {
 	route, err := routeLister.Routes("openshift-authentication").Get("oauth-openshift")
-	if err != nil && os.IsNotExist(err) {
-		return nil, []operatorv1.OperatorCondition{{
-			Type:    conditionPrefix + "Degraded",
-			Status:  operatorv1.ConditionTrue,
-			Reason:  "NotFound",
-			Message: fmt.Sprintf("The OAuth server route not found: %v", err),
-		}}
-	}
 	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil, []operatorv1.OperatorCondition{{
+				Type:    conditionPrefix + "Degraded",
+				Status:  operatorv1.ConditionTrue,
+				Reason:  "NotFound",
+				Message: "The OAuth server route 'openshift-authentication/oauth-openshift' was not found",
+			}}
+		}
+
 		return nil, []operatorv1.OperatorCondition{
 			{
 				Type:    conditionPrefix + "Degraded",
 				Status:  operatorv1.ConditionTrue,
 				Reason:  "GetFailed",
-				Message: fmt.Sprintf("Unable to get oauth-openshift route: %v", err),
+				Message: fmt.Sprintf("Unable to get 'openshift-authentication/oauth-openshift' route: %v", err),
 			},
 		}
 	}
