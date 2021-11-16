517c004649187ea2011d24170e00dd6d9eec9ba2
Damien Grisonnet <dgrisonn@redhat.com>
2021-11-16 17:04:17 +0100

    encryption-controller: sync secrets conditionally
    Update the version of library-go to include the latest changes that
    allow the encryption controller to synchronize the encryption-config
    secret only when the encryption controller should be run. This prevents
    the secrets from being unnecessarily deleted in the apiserver namespace
    whenever it doesn't exist in the config-managed namespace.
    
    Signed-off-by: Damien Grisonnet <dgrisonn@redhat.com>

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -535,7 +535,7 @@ func prepareOauthAPIServerOperator(ctx context.Context, controllerContext *contr
 	kubeInformers := certinformers.NewSharedInformerFactory(operatorCtx.kubeClient, resync)
 
 	nodeProvider := encryptiondeployer.NewDeploymentNodeProvider("openshift-oauth-apiserver", operatorCtx.kubeInformersForNamespaces)
-	deployer, err := encryptiondeployer.NewRevisionLabelPodDeployer("revision", "openshift-oauth-apiserver", operatorCtx.kubeInformersForNamespaces, operatorCtx.resourceSyncController, operatorCtx.kubeClient.CoreV1(), operatorCtx.kubeClient.CoreV1(), nodeProvider)
+	deployer, err := encryptiondeployer.NewRevisionLabelPodDeployer("revision", "openshift-oauth-apiserver", operatorCtx.kubeInformersForNamespaces, operatorCtx.kubeClient.CoreV1(), operatorCtx.kubeClient.CoreV1(), nodeProvider)
 	if err != nil {
 		return err
 	}
@@ -615,6 +615,7 @@ func prepareOauthAPIServerOperator(ctx context.Context, controllerContext *contr
 		operatorCtx.configClient.ConfigV1().APIServers(),
 		operatorCtx.operatorConfigInformer.Config().V1().APIServers(),
 		operatorCtx.kubeInformersForNamespaces,
+		operatorCtx.resourceSyncController,
 	).WithUnsupportedConfigPrefixForEncryptionControllers(
 		oauthapiconfigobservercontroller.OAuthAPIServerConfigPrefix,
 	).WithFinalizerController(
