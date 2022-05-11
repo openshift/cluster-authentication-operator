979eb11a40112b1ec3aa400bf98557b132487c22
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2022-05-11 13:51:15 +0200

    bindata/oauth-openshift: specify pod security level

--- pkg/operator/assets/bindata.go
+++ pkg/operator/assets/bindata.go
@@ -830,6 +830,9 @@ metadata:
     workload.openshift.io/allowed: "management"
   labels:
     openshift.io/cluster-monitoring: "true"
+    pod-security.kubernetes.io/enforce: privileged
+    pod-security.kubernetes.io/audit: privileged
+    pod-security.kubernetes.io/warn: privileged
 `)
 
 func oauthOpenshiftNsYamlBytes() ([]byte, error) {
