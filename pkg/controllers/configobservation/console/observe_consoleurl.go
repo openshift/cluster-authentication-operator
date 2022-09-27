dfb4b088fd911edab7dcb6b213b9f9f66e5a5326
Jakub Hadvig <jhadvig@redhat.com>
2022-09-28 00:34:25 +0200

    CONSOLE-3322: Update cluster-authentication-operator not to go degraded without console

--- pkg/controllers/configobservation/console/observe_consoleurl.go
+++ pkg/controllers/configobservation/console/observe_consoleurl.go
@@ -9,6 +9,8 @@ import (
 	"github.com/openshift/library-go/pkg/operator/configobserver"
 	"github.com/openshift/library-go/pkg/operator/events"
 
+	configv1 "github.com/openshift/api/config/v1"
+
 	"github.com/openshift/cluster-authentication-operator/pkg/controllers/configobservation"
 )
 
@@ -20,12 +22,28 @@ func ObserveConsoleURL(genericlisters configobserver.Listers, recorder events.Re
 	listers := genericlisters.(configobservation.Listers)
 	errs := []error{}
 
-	consoleConfig, err := listers.ConsoleLister.Get("cluster")
+	clusterVersionConfig, err := listers.ClusterVersionLister.Get("version")
 	if err != nil {
 		return existingConfig, append(errs, err)
 	}
 
+	isConsoleCapabilityEnabled := false
+	for _, capability := range clusterVersionConfig.Status.Capabilities.EnabledCapabilities {
+		if capability == configv1.ClusterVersionCapabilityConsole {
+			isConsoleCapabilityEnabled = true
+			break
+		}
+	}
+	if !isConsoleCapabilityEnabled {
+		return existingConfig, nil
+	}
+
+	consoleConfig, err := listers.ConsoleLister.Get("cluster")
+	if err != nil {
+		return existingConfig, append(errs, err)
+	}
 	observedAssetURL := consoleConfig.Status.ConsoleURL
+
 	if _, err := url.Parse(observedAssetURL); err != nil { // should never happen
 		return existingConfig, append(errs, fmt.Errorf("failed to parse consoleURL %q: %w", observedAssetURL, err))
 	}
