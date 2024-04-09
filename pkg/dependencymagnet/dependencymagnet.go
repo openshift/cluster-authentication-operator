bc67f4df315694c6c86946ac56021acb8421572a
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2024-04-09 13:15:35 +0200

    dependencymagnet: attract openshift/api/operator/v1/zz_generated.crd-manifests

--- pkg/dependencymagnet/dependencymagnet.go
+++ pkg/dependencymagnet/dependencymagnet.go
@@ -7,5 +7,6 @@ package dependencymagnet
 
 import (
 	_ "github.com/go-bindata/go-bindata/go-bindata"
+	_ "github.com/openshift/api/operator/v1/zz_generated.crd-manifests"
 	_ "github.com/openshift/build-machinery-go"
 )
