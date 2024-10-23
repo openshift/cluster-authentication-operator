79cb02d021d9abccbe20db67ab5289a968633f16
David Eads <deads@redhat.com>
2024-10-23 17:36:31 -0400

    refine output-resources

--- pkg/cmd/mom/output_resources_command.go
+++ pkg/cmd/mom/output_resources_command.go
@@ -16,53 +16,35 @@ func RunOutputResources(ctx context.Context) (*libraryoutputresources.OutputReso
 	return &libraryoutputresources.OutputResources{
 		ConfigurationResources: libraryoutputresources.ResourceList{
 			ExactResources: []libraryoutputresources.ExactResourceID{
-				exactResource("config.openshift.io", "ingresses", "", "cluster"),
+				libraryoutputresources.ExactResource("config.openshift.io", "ingresses", "", "cluster"),
 			},
 		},
 		ManagementResources: libraryoutputresources.ResourceList{
 			ExactResources: []libraryoutputresources.ExactResourceID{
-				exactResource("config.openshift.io", "clusteroperators", "", "authentication"),
-				exactResource("openshift.openshift.io", "authentications", "", "cluster"),
+				libraryoutputresources.ExactClusterOperator("authentication"),
+				libraryoutputresources.ExactResource("operator.openshift.io", "authentications", "", "cluster"),
 			},
 		},
 		UserWorkloadResources: libraryoutputresources.ResourceList{
 			ExactResources: []libraryoutputresources.ExactResourceID{
-				exactResource("", "secrets", "openshift-authentication", "v4-0-config-system-session"),
-				exactResource("", "secrets", "openshift-authentication", "v4-0-config-system-ocp-branding-template"),
-				exactResource("", "serviceaccounts", "openshift-authentication", "oauth-openshift"),
-				exactResource("apps", "deployments", "openshift-authentication", "oauth-openshift"),
-				exactResource("oauth.openshift.io", "oauthclients", "", "openshift-browser-client"),
-				exactResource("oauth.openshift.io", "oauthclients", "", "openshift-challenging-client"),
-				exactResource("oauth.openshift.io", "oauthclients", "", "openshift-cli-client"),
-				exactResource("rbac.authorization.k8s.io", "clusterrolebindings", "", "system:openshift:openshift-authentication"),
-				exactResource("rbac.authorization.k8s.io", "rolebindings", "openshift-config-managed", "system:openshift:oauth-servercert-trust"),
-				exactResource("rbac.authorization.k8s.io", "roles", "openshift-config-managed", "system:openshift:oauth-servercert-trust"),
+				libraryoutputresources.ExactSecret("openshift-authentication", "v4-0-config-system-session"),
+				libraryoutputresources.ExactSecret("openshift-authentication", "v4-0-config-system-ocp-branding-template"),
+				libraryoutputresources.ExactServiceAccount("openshift-authentication", "oauth-openshift"),
+				libraryoutputresources.ExactDeployments("openshift-authentication", "oauth-openshift"),
+				exactOAuthClient("openshift-browser-client"),
+				exactOAuthClient("openshift-challenging-client"),
+				exactOAuthClient("openshift-cli-client"),
+				libraryoutputresources.ExactClusterRoleBinding("system:openshift:openshift-authentication"),
+				libraryoutputresources.ExactRoleBinding("openshift-config-managed", "system:openshift:oauth-servercert-trust"),
+				libraryoutputresources.ExactRole("openshift-config-managed", "system:openshift:oauth-servercert-trust"),
 			},
 			GeneratedNameResources: []libraryoutputresources.GeneratedResourceID{
-				generatedResource("certificates.k8s.io", "certificatesigningrequests", "", "system:openshift:openshift-authenticator-"),
+				libraryoutputresources.GeneratedCSR("system:openshift:openshift-authenticator-"),
 			},
 		},
 	}, nil
 }
 
-func exactResource(group, resource, namespace, name string) libraryoutputresources.ExactResourceID {
-	return libraryoutputresources.ExactResourceID{
-		OutputResourceTypeIdentifier: libraryoutputresources.OutputResourceTypeIdentifier{
-			Group:    group,
-			Resource: resource,
-		},
-		Namespace: namespace,
-		Name:      name,
-	}
-}
-
-func generatedResource(group, resource, namespace, name string) libraryoutputresources.GeneratedResourceID {
-	return libraryoutputresources.GeneratedResourceID{
-		OutputResourceTypeIdentifier: libraryoutputresources.OutputResourceTypeIdentifier{
-			Group:    group,
-			Resource: resource,
-		},
-		Namespace:     namespace,
-		GeneratedName: name,
-	}
+func exactOAuthClient(name string) libraryoutputresources.ExactResourceID {
+	return libraryoutputresources.ExactResource("oauth.openshift.io", "oauthclients", "", name)
 }
