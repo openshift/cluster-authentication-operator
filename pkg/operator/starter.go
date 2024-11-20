50646a700ceb85ff2eee32108f887d336cafe175
Ilias Rinis <irinis@redhat.com>
2024-11-20 10:40:15 +0100

    deployment: bypass precondition checks if OIDC is available
    This allows the controller to proceed with the sync and
    eventually scale down the deployment to 0 replicas.

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -256,6 +256,8 @@ func prepareOauthOperator(
 		authOperatorInput.eventRecorder,
 		versionRecorder,
 		informerFactories.kubeInformersForNamespaces.InformersFor("openshift-authentication"),
+		informerFactories.operatorInformer.Operator().V1().KubeAPIServers(),
+		informerFactories.kubeInformersForNamespaces.InformersFor("openshift-kube-apiserver"),
 	)
 
 	workersAvailableController := ingressnodesavailable.NewIngressNodesAvailableController(
