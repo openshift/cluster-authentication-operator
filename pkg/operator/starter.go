33531a4388699b22ed41d9c35598f19cf5fc0d31
Ilias Rinis <irinis@redhat.com>
2024-11-20 10:40:15 +0100

    deployment: bypass precondition checks if OIDC is available
    This allows the controller to ignore missing preconditions in case
    of OIDC to avoid failing on resources that have been deleted.

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -261,6 +261,8 @@ func prepareOauthOperator(
 		authOperatorInput.eventRecorder,
 		versionRecorder,
 		informerFactories.kubeInformersForNamespaces.InformersFor("openshift-authentication"),
+		informerFactories.operatorInformer.Operator().V1().KubeAPIServers(),
+		informerFactories.kubeInformersForNamespaces.InformersFor("openshift-kube-apiserver"),
 	)
 
 	workersAvailableController := ingressnodesavailable.NewIngressNodesAvailableController(
