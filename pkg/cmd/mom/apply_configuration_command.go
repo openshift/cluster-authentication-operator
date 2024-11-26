456a46c01065e2367fb3159b9c242f9df5a636e6
David Eads <deads@redhat.com>
2024-11-26 11:50:50 -0500

    MOM fitting

--- pkg/cmd/mom/apply_configuration_command.go
+++ pkg/cmd/mom/apply_configuration_command.go
@@ -14,19 +14,14 @@ func NewApplyConfigurationCommand(streams genericiooptions.IOStreams) *cobra.Com
 	return libraryapplyconfiguration.NewApplyConfigurationCommand(RunApplyConfiguration, runOutputResources, streams)
 }
 
-func RunApplyConfiguration(ctx context.Context, input libraryapplyconfiguration.ApplyConfigurationInput) (libraryapplyconfiguration.AllDesiredMutationsGetter, error) {
+func RunApplyConfiguration(ctx context.Context, input libraryapplyconfiguration.ApplyConfigurationInput) (*libraryapplyconfiguration.ApplyConfigurationRunResult, libraryapplyconfiguration.AllDesiredMutationsGetter, error) {
 	authenticationOperatorInput, err := operator.CreateOperatorInputFromMOM(ctx, input)
 	if err != nil {
-		return nil, fmt.Errorf("unable to configure operator input: %w", err)
+		return nil, nil, fmt.Errorf("unable to configure operator input: %w", err)
 	}
 	operatorStarter, err := operator.CreateOperatorStarter(ctx, authenticationOperatorInput)
 	if err != nil {
-		return nil, fmt.Errorf("unable to configure operators: %w", err)
+		return nil, nil, fmt.Errorf("unable to configure operators: %w", err)
 	}
-	var operatorRunError error
-	if err := operatorStarter.RunOnce(ctx); err != nil {
-		operatorRunError = fmt.Errorf("unable to run operators: %w", err)
-	}
-
-	return libraryapplyconfiguration.NewApplyConfigurationFromClient(input.MutationTrackingClient.GetMutations()), operatorRunError
+	return operatorStarter.RunOnce(ctx, input)
 }
