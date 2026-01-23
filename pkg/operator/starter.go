967711b7e27bbd420b23db5cb7d550aa426843ba
Ilias Rinis <irinis@redhat.com>
2026-01-23 10:13:39 +0100

    bindata: add oauth-server pod and namespace network policies

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -142,6 +142,8 @@ func prepareOauthOperator(
 		[]string{ // required resources
 			"oauth-openshift/audit-policy.yaml",
 			"oauth-openshift/ns.yaml",
+			"oauth-openshift/networkpolicy_oauth-server.yaml",
+			"oauth-openshift/networkpolicy_namespace_default-deny-all.yaml",
 			"oauth-openshift/authorization.openshift.io_rolebindingrestrictions.yaml",
 		},
 		resourceapply.NewKubeClientHolder(authOperatorInput.kubeClient).WithAPIExtensionsClient(authOperatorInput.apiextensionClient),
